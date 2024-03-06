2cdb3fc615364cf00477bc367a48de14d83ec500
PSH <pshysd@kakao.com>
2024-03-06 12:43:06 +0900

    ADD: patch, delete 라우팅 추가

--- routes/users.ts
+++ routes/users.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { isLoggedIn } from '../middlewares';
-import { getCounselingsByUserId } from '../controllers/users';
+import { deleteUser, getCounselingsByUserId, updateUser } from '../controllers/users';
 
 export const router = express.Router();
 
 router.get('/counselings/:id', isLoggedIn, getCounselingsByUserId);
+
+router.patch('/', isLoggedIn, updateUser);
+
+router.delete('/', isLoggedIn, deleteUser);
 
 export default router;
