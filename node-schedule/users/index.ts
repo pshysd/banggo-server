8654f21901339338a318b3183ba53cd796694d83
PSH <pshysd@kakao.com>
2024-03-06 12:42:30 +0900

    MODIFY: 스케쥴링 로직 수정

--- node-schedule/users/index.ts
+++ node-schedule/users/index.ts
@@ -1,17 +1,26 @@
+import { Op } from 'sequelize';
 import User from '../../models/user';
 import dayjs from 'dayjs';
 
-const today = dayjs();
-today.format('yyyy-MM-DD HH:mm:ss');
-
 const forcedDeleteUser = async () => {
 	if (process.env.INSTANCE_ID === '0') {
-		await User.destroy({
-			where: { deletedAt: today.subtract(7, 'day') },
-			force: true,
+		const today = dayjs();
+		const oneWeekAgo = today.subtract(7, 'day').format('yyyy-MM-DD HH:mm:ss');
+		const deletedUsers = await User.findAll({
+			where: {
+				deletedAt: {
+					[Op.lt]: oneWeekAgo,
+				},
+			},
 		});
-	} else {
-		console.log('요청을 확인했으나 실행되지 않았음');
+
+		if (deletedUsers.length !== 0) {
+			deletedUsers.map((user) => {
+				user.destroy({
+					force: true,
+				});
+			});
+		}
 	}
 };
 
